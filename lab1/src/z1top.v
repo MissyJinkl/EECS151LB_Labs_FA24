65a5bb66642ef46cedcc18b187a6c42aed6f15f8
Missy Jin <missy_jin@berkeley.edu>
2024-09-13 09:06:22 -0700

    update LED

--- lab1/src/z1top.v
+++ lab1/src/z1top.v
@@ -4,6 +4,7 @@ module z1top(
   input [1:0] SWITCHES,
   output [5:0] LEDS
 );
+  assign LEDS[1] = BUTTONS[0] & BUTTONS[1] & BUTTONS[2] & BUTTONS[3];
   and(LEDS[0], BUTTONS[0], SWITCHES[0]);
-  assign LEDS[5:1] = 0;
+  assign LEDS[5:2] = 0;
 endmodule
